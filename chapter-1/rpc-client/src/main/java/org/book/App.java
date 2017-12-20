b490e689eb48adda4bd0cae51aaf4f33418d7323
rico <gongroc@gmail.com>
2017-12-20 14:56:59 +0800

    添加注释

--- chapter-1/rpc-client/src/main/java/org/book/App.java
+++ chapter-1/rpc-client/src/main/java/org/book/App.java
@@ -6,7 +6,9 @@ import java.net.InetSocketAddress;
 
 public class App {
     public static void main(String[] args) {
+        //调用服务
         IHello service = Client.get(IHello.class, new InetSocketAddress("localhost", 8020));
+        //打印服务内容
         System.out.println(service.say("RPC"));
     }
 }
