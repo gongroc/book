45d7cd95c4210ccf696b0e5d78a99dae286f9c2b
rico <gongroc@gmail.com>
2017-12-20 20:09:06 +0800

    完善

--- chapter-5/cloud-admin/src/main/java/org/book/rpc/cloud/admin/CloudAdminApplication.java
+++ chapter-5/cloud-admin/src/main/java/org/book/rpc/cloud/admin/CloudAdminApplication.java
@@ -1,8 +1,12 @@
 package org.book.rpc.cloud.admin;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableAdminServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CloudAdminApplication {
 
--- chapter-5/cloud-client/src/main/java/org/book/rpc/cloud/client/controller/Controller.java
+++ chapter-5/cloud-client/src/main/java/org/book/rpc/cloud/client/controller/Controller.java
@@ -5,8 +5,10 @@ import org.book.rpc.cloud.client.service.BusinessService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@RestController
 public class Controller {
 
 
