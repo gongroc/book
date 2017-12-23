a924a01d9479204f6478e5a3fe9d84db411db882
rico <gongroc@gmail.com>
2017-12-23 20:06:47 +0800

    规整swagger2配置方式

--- chapter-16/cloud-gateway/src/main/java/org/book/rpc/cloud/gateway/configuration/SwaggerConfiguration.java
+++ chapter-16/cloud-gateway/src/main/java/org/book/rpc/cloud/gateway/configuration/SwaggerConfiguration.java
@@ -1,7 +1,6 @@
 package org.book.rpc.cloud.gateway.configuration;
 
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
--- chapter-16/dubbo-swagger/src/main/java/org/book/rpc/dubbo/swagger/configuration/Swagger2Configuration.java
+++ chapter-16/dubbo-swagger/src/main/java/org/book/rpc/dubbo/swagger/configuration/Swagger2Configuration.java
@@ -1,5 +1,8 @@
 package org.book.rpc.dubbo.swagger.configuration;
 
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -10,28 +13,32 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 @Configuration
 @EnableSwagger2
 public class Swagger2Configuration {
 
     @Bean
-    public Docket createRestApi() {
+    public Docket createRestApi() throws IOException, XmlPullParserException {
+        MavenXpp3Reader reader = new MavenXpp3Reader();
+        Model model = reader.read(new FileReader("pom.xml"));
+
+        ApiInfo apiInfo = new ApiInfoBuilder()
+                .title("接口名称")
+                .description("接口介绍内容")
+                .version(model.getVersion())
+                .build();
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
+                .apiInfo(apiInfo)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("org.book"))
                 .paths(PathSelectors.any())
                 .build();
     }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Spring Boot中使用Swagger2构建RESTful APIs")
-                .description("在Dubbo中将此配置在网关模块中")
-                .termsOfServiceUrl("https://github.com/gongroc/book")
-                .version("1.0")
-                .build();
-    }
 
 
 }
